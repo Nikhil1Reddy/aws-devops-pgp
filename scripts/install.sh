b37779cf4e12861f407f9350b39c55828e095f3e
nikhil1reddy <nikhilreddy64@gmail.com>
2025-05-02 00:08:51 +0000

    Add CodeDeploy hooks

---
+++ scripts/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt-get update -y
+apt-get install -y python3-pip
+pip3 install flask pymysql boto3
+
---
+++ scripts/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ubuntu/aws-devops-pgp
+nohup python3 app.py > app.log 2>&1 &
+
